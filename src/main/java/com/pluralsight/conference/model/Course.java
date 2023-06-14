9cf295d4bd463b25d2c099d82a0c8c00a75437e6
Velja <velimir.patrnogic@gmail.com>
2023-06-14 12:57:44 +0200

    Added lombok, removed boilerplate code

--- src/main/java/com/pluralsight/conference/model/Course.java
+++ src/main/java/com/pluralsight/conference/model/Course.java
@@ -2,10 +2,12 @@ package com.pluralsight.conference.model;
 
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
+import lombok.Data;
 
 import javax.persistence.*;
 
 @Entity
+@Data
 @Table(name = "COURSE")
 public class Course {
 
@@ -22,36 +24,4 @@ public class Course {
     @JsonBackReference
     @ManyToOne
     private Registration registration;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Registration getRegistration() {
-        return registration;
-    }
-
-    public void setRegistration(Registration registration) {
-        this.registration = registration;
-    }
 }
--- src/main/java/com/pluralsight/conference/model/RegistrationReport.java
+++ src/main/java/com/pluralsight/conference/model/RegistrationReport.java
@@ -1,38 +1,17 @@
 package com.pluralsight.conference.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.AssociationOverride;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class RegistrationReport {
 
     private String name;
     private String courseName;
     private String courseDescription;
-
-    public RegistrationReport(String name, String courseName, String courseDescription) {
-        this.name = name;
-        this.courseName = courseName;
-        this.courseDescription = courseDescription;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getCourseName() {
-        return courseName;
-    }
-
-    public void setCourseName(String courseName) {
-        this.courseName = courseName;
-    }
-
-    public String getCourseDescription() {
-        return courseDescription;
-    }
-
-    public void setCourseDescription(String courseDescription) {
-        this.courseDescription = courseDescription;
-    }
 }
